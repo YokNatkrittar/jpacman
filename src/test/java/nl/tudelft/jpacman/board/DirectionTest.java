49467a14c2b59d1b482c1528d307fb7dc9f04bd6
Yok <yokojournal.co@gmail.com>
2026-08-27 20:18:49 +0700

    Add testCase to DirectionTest

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -19,4 +19,21 @@ public class DirectionTest {
         Direction north = Direction.valueOf("NORTH");
         assertThat(north.getDeltaY()).isEqualTo(-1);
     }
+
+    @Test
+    void testSouth() {
+        Direction south = Direction.valueOf("SOUTH");
+        assertThat(south.getDeltaY()).isEqualTo(1);
+    }
+    @Test
+    void testEast() {
+        Direction east = Direction.valueOf("EAST");
+        assertThat(east.getDeltaY()).isEqualTo(0);
+    }
+
+    @Test
+    void testWest() {
+        Direction west = Direction.valueOf("WEST");
+        assertThat(west.getDeltaY()).isEqualTo(0);
+    }
 }
